450b8ff8fdb62da8366c59cf59a002416ea9cba4
Evan Ning <evanning0206@gmail.com>
2024-01-26 02:46:37 -0500

    first commit

--- app.py
+++ app.py
@@ -6,29 +6,38 @@ from email.header import decode_header
 from email.utils import parseaddr
 
 from openai import OpenAI
-client = OpenAI(api_key = "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")
+client = OpenAI(api_key = "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")
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
-def index():
-    return render_template('emails.html')
+def home():
+    return render_template('index.html')
 
 @app.route('/emails')
+def emails():
+    return render_template('emails.html')
+
+@app.route('/howto')
+def howto():
+    return render_template('howto.html')
+
+@app.route('/get_emails')
 def get_emails():
     age = str(request.args.get('age'))
     occupation = str(request.args.get('occupation'))
-
+    username = str(request.args.get('email'))
+    password = str(request.args.get('password'))
     # Update these values with your email server details
-    email_address = 'yoppyenasia123@gmail.com'
-    email_password = 'rewv aivp cgxv sefo'
+    # test yoppyenasia123@gmail.com
+    # test rewv aivp cgxv sefo 
     imap_server = 'imap.gmail.com'
     imap_port = 993
     try:
         # Connect to the IMAP server
         mail = imaplib.IMAP4_SSL(imap_server)
-        mail.login(email_address, email_password)
+        mail.login(username, password)
         mail.select("Inbox")
 
         # Search for all unseen emails
@@ -54,18 +63,18 @@ def get_emails():
                     })
 
         ordered_emails = []
-        prompt = "You are an email manager for a " + age + " year old " + occupation + ". Your job is to order the persons inbox by the title, sender, and date of the emails, so that the most important emails for them to read, given their age and occupation, are at the top. Also provide a one sentence explanation at the beginning justifying why the order of the emails is as is. Here are the following emails: \n"
+        systemmessage = "You are an email manager for a " + age + " year old " + occupation + ". Your job is to order the persons inbox by the title, sender, and date of the emails, so that the most important emails for them to read, given their age and occupation, are at the top. Also provide a one sentence explanation at the beginning justifying why the order of the emails is as is."
+        usermessage = ""
         index = 1
         for thismsg in email_list:
-            prompt += str(index) + " Title: " + thismsg['subject'] + " Sender: " + thismsg['from'] + " Date recieved: " + thismsg['date'] + "\n"
+            usermessage += str(index) + " Title: " + thismsg['subject'] + " Sender: " + thismsg['from'] + " Date recieved: " + thismsg['date'] + "\n"
             index += 1
         
-        prompt += "Add new lines between each email and sentence."
         response = client.chat.completions.create(
                 model="gpt-3.5-turbo-1106",
                 messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt}
+                {"role": "system", "content": systemmessage},
+                {"role": "user", "content": usermessage}
                 ]
             )
         response = str(response.choices[0].message.content)
